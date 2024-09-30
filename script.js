89b0af93f83eb448750c535972ca06c361d425b7
Luiz Luna <luizluna.dev@gmail.com>
2024-09-30 14:10:19 -0300

    Create all 16 square divs

---
+++ script.js
@@ -0,0 +1,11 @@
+const canvasContainer = document.querySelector("#canvas.container");
+
+function createDivs(num) {
+    for (i = 0; i < num; i++) {
+        const grid = document.createElement("div");
+        grid.classList.add("pixel-div");
+        canvasContainer.appendChild(grid);
+    }
+}
+
+createDivs(16);
