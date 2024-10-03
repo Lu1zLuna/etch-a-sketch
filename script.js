14c90fdac10b3cb9d622965b7703c81923050afd
Luiz Luna <luizluna.dev@gmail.com>
2024-10-03 12:07:34 -0300

    Fix CreateDivs function to use the correct formula

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const canvasContainer = document.querySelector("#canvas.container");
 
 function createDivs(num) {
-    for (i = 0; i < num; i++) {
+    for (i = 0; i < num * num; i++) {
         const grid = document.createElement("div");
         grid.classList.add("pixel-div");
         canvasContainer.appendChild(grid);
     }
 }
 
-createDivs(256);
+createDivs(20);
 
-const gridList = document.querySelectorAll(".pixel-grid");
+const gridList = document.querySelectorAll(".pixel-div");
 
 gridList.forEach(function (elem) {
-    elem.addEventListener("hover", function () {
+    elem.addEventListener("mouseenter", function () {
         // to be implemented
     })
 })
