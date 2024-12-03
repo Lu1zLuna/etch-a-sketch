2fc41bdb881115f0ddbb464e1c681c16ad9aff20
Luiz Luna <luizluna.dev@gmail.com>
2024-12-02 22:13:47 -0300

    Create reset-btn in index.html instead of script.js

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const canvasContainer = document.querySelector("#canvas.container");
 
 function createDivs(num) {
-    for (i = 0; i < num * num; i++) {
+    canvasContainer.innerHTML = "";
+
+    for (let i = 0; i < num * num; i++) {
         const grid = document.createElement("div");
-        grid.classList.add("pixel-div");
+        grid.classList.add("paintable-div");
         canvasContainer.appendChild(grid);
     }
 }
 
-createDivs(20);
+let squaresNumber = 16;
 
-const gridList = document.querySelectorAll(".pixel-div");
+const paintableGrid = document.querySelectorAll(".paintable-div");
 
-gridList.forEach(function (elem) {
+paintableGrid.forEach(function (elem) {
     elem.addEventListener("mouseenter", function () {
         elem.setAttribute("style", "background-color: black;");
     })
-})
+});
+
+const button = document.querySelector("#reset-btn");
+
+button.addEventListener("click", () => {
+    let squaresNumber = parseInt(prompt("How many squares per side do you want for the new grid?"))
+
+    if (Number.isInteger(squaresNumber) && squaresNumber > 0) {
+        createDivs(squaresNumber); // Create a new grid with the specified size
+    } else {
+        alert("Please enter a number between 1 and 100.");
+    }
+});
+
+createDivs(squaresNumber);
